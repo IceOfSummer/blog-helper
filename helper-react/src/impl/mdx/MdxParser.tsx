adfe879836097a2fa53fd81afe698e78f4100023
IceOfSummer <xu2237803016@outlook.com>
2024-12-10 15:56:12 +0800

    chore: 整理依赖

--- helper-react/src/impl/mdx/MdxParser.tsx
+++ helper-react/src/impl/mdx/MdxParser.tsx
@@ -1,5 +1,5 @@
 import type { MarkdownParser, ParsedMarkdown } from '../../types'
-import type {ReactNode} from 'react'
+import React, {ReactNode} from 'react'
 import { compile, run } from '@mdx-js/mdx'
 import * as runtime from 'react/jsx-runtime'
 import * as os from "node:os";
@@ -7,27 +7,6 @@ import createCommonHeadingWithId from './CommonHeadingWithId'
 import adjustToc from '../../common/toc-adjust-plugin'
 import remarkGfm from 'remark-gfm'
 
-const components = {
-  h1: createCommonHeadingWithId('h1'),
-  h2: createCommonHeadingWithId('h2'),
-  h3: createCommonHeadingWithId('h3'),
-  h4: createCommonHeadingWithId('h4'),
-  h5: createCommonHeadingWithId('h5'),
-}
-
-
-async function parseMarkdownContent0(content: string): Promise<ReactNode> {
-  const code = String(await compile(content, { outputFormat: 'function-body', remarkPlugins: [adjustToc, remarkGfm] }))
-
-  const { default: MDXContent } = await run(code, {
-    ...runtime,
-    // @ts-ignore
-    baseUrl: import.meta.url,
-  })
-
-  // Render the MDX content, supplying the ClientComponent as a component
-  return <MDXContent components={components} />
-}
 
 type ParseError = Error & {
   line: number
@@ -56,38 +35,68 @@ function takeAboveLines(self: string[], lines: string[], startLine: number, line
   }
 }
 
-/**
- * 处理 html 博客内容
- */
-async function parseMarkdownContent(content: string): Promise<React.ReactNode> {
-  try {
-    return await parseMarkdownContent0(content)
-  } catch (e) {
-    if (!isParseAeeror(e)) {
-      return Promise.reject(e)
+export type MdxParserConfig = {
+  components: Record<string, React.ComponentType<any>>
+}
+
+function createMdxParser(config?: MdxParserConfig): MarkdownParser {
+
+  const components = {
+    h1: createCommonHeadingWithId('h1'),
+    h2: createCommonHeadingWithId('h2'),
+    h3: createCommonHeadingWithId('h3'),
+    h4: createCommonHeadingWithId('h4'),
+    h5: createCommonHeadingWithId('h5'),
+    ...config?.components,
+  }
+
+
+  async function parseMarkdownContent0(content: string): Promise<ReactNode> {
+    const code = String(await compile(content, { outputFormat: 'function-body', remarkPlugins: [adjustToc, remarkGfm] }))
+
+    const { default: MDXContent } = await run(code, {
+      ...runtime,
+      // @ts-ignore
+      baseUrl: import.meta.url,
+    })
+
+    // Render the MDX content, supplying the ClientComponent as a component
+    return <MDXContent components={components} />
+  }
+
+  /**
+   * 处理 html 博客内容
+   */
+  async function parseMarkdownContent(content: string): Promise<React.ReactNode> {
+    try {
+      return await parseMarkdownContent0(content)
+    } catch (e) {
+      if (!isParseAeeror(e)) {
+        return Promise.reject(e)
+      }
+      // indicate where is wrong.
+      const lines = content.split(os.EOL)
+      const message = ['Failed to parse markdown content:']
+      const programLine = e.line - 1
+
+      message.push('')
+      takeAboveLines(message, lines, programLine, 4)
+
+      message.push('        ' + (new Array(e.column - 1).join(' ')) + '^')
+      message.push('\t' + e.message)
+
+
+      return Promise.reject(new Error(message.join(os.EOL)))
     }
-    // indicate where is wrong.
-    const lines = content.split(os.EOL)
-    const message = ['Failed to parse markdown content:']
-    const programLine = e.line - 1
-
-    message.push('')
-    takeAboveLines(message, lines, programLine, 4)
-
-    message.push('        ' + (new Array(e.column - 1).join(' ')) + '^')
-    message.push('\t' + e.message)
+  }
 
 
-    return Promise.reject(new Error(message.join(os.EOL)))
+  return  {
+    async parse(markdown: string): Promise<ParsedMarkdown> {
+
+      return await parseMarkdownContent(markdown)
+    }
   }
 }
 
-
-const mdxParser: MarkdownParser = {
-  async parse(markdown: string): Promise<ParsedMarkdown> {
-
-    return await parseMarkdownContent(markdown)
-  }
-}
-
-export default mdxParser
+export default createMdxParser
